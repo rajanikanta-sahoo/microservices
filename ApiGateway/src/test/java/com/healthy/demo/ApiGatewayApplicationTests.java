f1b809bf0141f213e4f51e471669117700a30552
Lulu <rajanikanta.sahoo@gmail.com>
2021-07-16 19:58:01 +0530

    initial commit

---
+++ ApiGateway/src/test/java/com/healthy/demo/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
