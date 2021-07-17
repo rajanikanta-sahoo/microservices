fde3503c3648e121254db1413f1b229e68c38362
Lulu <rajanikanta.sahoo@gmail.com>
2021-07-17 09:04:33 +0530

    initial commit

---
+++ Hosptal/src/main/java/com/healthy/hospital/HosptalApplication.java
@@ -0,0 +1,13 @@
+package com.healthy.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosptalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosptalApplication.class, args);
+	}
+
+}
---
+++ Hosptal/src/test/java/com/healthy/hospital/HosptalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthy.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosptalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
